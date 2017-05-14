6c4577d41464dd562d42ac314d1f3c68c96af621
mahagony4 <mahagony4@gmail.com>
2017-05-14 15:43:03 +0200

    GET Method

--- server.js
+++ server.js
@@ -33,6 +33,20 @@ app.get("/ab*cd", (req, res) => {
 	res.send("Page Pattern Match\n");
 });
 
+app.get("/index.html", (req, res) => {
+	res.sendFile(__dirname + "/" + "index.html");
+});
+
+app.get("/process_get", (req, res) => {
+	// Prepare output in JSON format
+	response = {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name,
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
+});
+
 const server = app.listen(8081, () => {
 	let host = server.address().address;
 	let port = server.address().port;
